d2c334506123a7a354487a0b3b2dedfa5b7135f7
soumya-ganesh <119743023+soumya-ganesh@users.noreply.github.com>
2025-11-05 11:53:35 +0530

    bow.py

--- bow.py
+++ bow.py
@@ -18,7 +18,7 @@ def show_left_aligned(df):
     )
 
 # --- Input Section ---
-st.subheader("Enter Three Documents")
+st.subheader("Enter Three Documents. Type the documents without a fullstop at the end.")
 doc1 = st.text_area("Document 1")
 doc2 = st.text_area("Document 2")
 doc3 = st.text_area("Document 3")
